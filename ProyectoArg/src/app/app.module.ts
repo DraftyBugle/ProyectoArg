b051a8fe1d06e66846d5a09495ff9cbb275de30d
DraftyBugle <fedeterrezas@gmail.com>
2022-08-04 16:10:45 -0300

    final frondt end sin edicion mia

--- ProyectoArg/src/app/app.module.ts
+++ ProyectoArg/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgpgComponent } from './components/logo-argpg/logo-argpg.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoArgpgComponent,
-    SocialComponent,
     BannerComponent,
     AcercadeComponent,
     ExperienciaComponent,
